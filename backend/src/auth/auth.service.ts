5149053e9dee642841191b933806dbc9a206b220
denosaur98 <denischurchill1998@gmail.com>
2025-05-11 11:12:01 +0300

    добавил хэширование паролей, добавил dotenv переменные

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { ConflictException, Injectable } from '@nestjs/common';
 import { PrismaClient } from 'generated/prisma';
 import { RegisterRequest } from './dto/register.dto';
+import { hash } from 'argon2';
 
 @Injectable()
 export class AuthService {
@@ -23,7 +24,7 @@ export class AuthService {
       data: {
         name,
         email,
-        password,
+        password: await hash(password),
       },
     });
 
